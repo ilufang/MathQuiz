e05eb2bb0a128db7df8469c6c0b5c6d9ca59fb43
ilufang <cc2lufang@gmail.com>
2014-07-15 15:22:55 -0700

    Commit

---
+++ game.js
@@ -0,0 +1,42 @@
+if (jQuery)
+{
+	var checkAnswers = function()
+	{
+		var answerStr = "";
+		var answers = $(":checked");
+		var score = 0;
+		var count = 0;
+		var correctAnswers = new Array(6);
+		correctAnswers[0] = "b";
+		correctAnswers[1] = "d";
+		correctAnswers[2] = "c";
+		correctAnswers[3] = "a";
+		correctAnswers[4] = "b";
+		correctAnswers[5] = "c";
+		answers.each(function(i)
+		{
+			count++;
+			//alert(correctAnswers[i]);
+			//alert(answers[i]);
+			if (correctAnswers[i]==answers[i].value)
+			{
+				score++;
+			}
+		});
+		if (count==6)
+		{
+			// Finished, display score
+			if(score==6)
+			{
+			// Perfect
+			$("body").addClass("perfect");
+			$("h1").text("All answers are correct. You WIN!!!");
+			}else
+			{
+			// Display score
+			$("body").addClass("done");
+			$("h1").text("You answered "+score+" questions correctly.");
+			}
+		}
+	}
+}
